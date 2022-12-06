73519202400454b3e2b72548408c5577eb677723
= <=>
2022-12-06 17:39:40 +0100

    local storage for cart items

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -9,15 +11,18 @@ import Home from "./components/pages/Home";
 import SignUp from "./components/pages/SignUp";
 import Login from "./components/pages/Login";
 import Products from "./components/pages/Products";
+import Cart from "./components/pages/Cart";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ToastContainer />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
         </Routes>
--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
-  cartItems: [],
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
   cartTotalQuantity: 0,
   cartTotalAmount: 0,
 };
@@ -11,19 +14,29 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-      const existingIndex = state.cartItems.findIndex(
+      const itemIndex = state.cartItems.findIndex(
         (item) => item.id === action.payload.id
       );
 
-      if (existingIndex >= 0) {
-        state.cartItems[existingIndex] = {
-          ...state.cartItems[existingIndex],
-          cartQuantity: state.cartItems[existingIndex].cartQuantity + 1,
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex] = {
+          ...state.cartItems[itemIndex],
+          cartQuantity: state.cartItems[itemIndex].cartQuantity + 1,
         };
+        toast.info(
+          `Increased ${state.cartItems[itemIndex].name} cart quantity`,
+          {
+            position: "bottom-left",
+          }
+        );
       } else {
         let tempProductItem = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(tempProductItem);
+        toast.success(`Added ${action.payload.name} to cart`, {
+          position: "bottom-left",
+        });
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
